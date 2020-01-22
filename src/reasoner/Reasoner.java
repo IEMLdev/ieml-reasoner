fed5dc4cc0b945dbcabd14b5c2517f05c29d5c74
Vincent Letard <letard.vincent@gmail.com>
2020-01-21 23:19:38 -0500

    Created word parser up to polymorphemes.

---
+++ src/reasoner/Reasoner.java
@@ -0,0 +1,26 @@
+package reasoner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import parser.JSONParser;
+import util.Tuple;
+
+public class Reasoner{
+  public static void main(String[] args) throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File("resources/words_sample.json"));
+    scanner.useDelimiter("\\A");
+    JSONArray arr = new JSONArray(scanner.next());
+    scanner.close();
+    
+    for (int i = 0; i < arr.length(); i++) {
+      JSONObject obj = arr.getJSONObject(i);
+      Tuple<Object> t = JSONParser.parseJSON(obj);
+      System.out.println(i + " " + t);
+    }
+  }
+}
