9370ca299de1de8f1ab85c027cdb33eb111e82c0
Vincent Letard <letard.vincent@gmail.com>
2020-03-11 21:03:16 -0400

    Detailed and clarified system execution.

--- src/reasoner/Reasoner.java
+++ src/reasoner/Reasoner.java
@@ -27,6 +27,7 @@ import parser.Word;
 public class Reasoner{
   private final Dictionary dict;
   private final ArrayList<Word> words;
+  String verifyingStats, solvingStats;
 
   public Reasoner(InputStream wordStream, InputStream dictStream) throws JSONStructureException, StyleException {
     Scanner scanner = null;
@@ -61,23 +62,24 @@ public class Reasoner{
     }
   }
 
-  public HashMap<String, LinkedList<String>> searchPolysemy(String lang) throws MissingTranslationException {
-    HashMap<String, LinkedList<String>> m = new HashMap<String, LinkedList<String>>();
-    HashMap<String, LinkedList<String>> multiple = new HashMap<String, LinkedList<String>>();
+  public HashMap<String, LinkedList<Word>> searchPolysemy(String lang) throws MissingTranslationException {
+    HashMap<String, LinkedList<Word>> iemlWordsPerNLWord = new HashMap<String, LinkedList<Word>>();
+    HashMap<String, LinkedList<Word>> multiple = new HashMap<String, LinkedList<Word>>();
 
     for (Word w: this.words) {
       for (String singleTranslation: this.dict.getFromUSL(w.getUsl()).get(lang)) {
-        m.putIfAbsent(singleTranslation, new LinkedList<String>());
-        LinkedList<String> l = m.get(singleTranslation);
-        l.add(dict.getFromUSL(w.getUsl()).get(lang).toString());
-        if (l.size() == 2)
-          multiple.put(singleTranslation, l);
+        iemlWordsPerNLWord.putIfAbsent(singleTranslation, new LinkedList<Word>());
+        LinkedList<Word> iemlWordList = iemlWordsPerNLWord.get(singleTranslation);
+        iemlWordList.add(w);
+        if (iemlWordList.size() == 2)
+          multiple.put(singleTranslation, iemlWordList);
       }
     }
     return multiple;
   }
 
   public LinkedList<String> computeDBProportions() throws MissingTranslationException{
+    LinkedList<Long> durations = new LinkedList<Long>();
     LinkedList<String> validProportions = new LinkedList<String>();
 
     for (int i = 0; i < words.size(); i++) {
@@ -85,6 +87,7 @@ public class Reasoner{
         for (int k = j+1; k < words.size(); k++) {
           for (int l = i+1; l < words.size(); l++) {
             if (!((i == j && k == l) || (i == k && j == l) || (i == j && j == k && k == l))) {
+              long start = System.currentTimeMillis();
               DefaultProportion<Word> p = new DefaultProportion<Word>(words.get(i), words.get(j), words.get(k), words.get(l));
               if (p.isValid()) {
                 validProportions.add(this.dict.getFromUSL(words.get(i).getUsl()).get("fr") + " : "
@@ -92,15 +95,36 @@ public class Reasoner{
                     + this.dict.getFromUSL(words.get(k).getUsl()).get("fr") + " : "
                     + this.dict.getFromUSL(words.get(l).getUsl()).get("fr"));
               }
+              durations.add(System.currentTimeMillis() - start);
             }
           }
         }
       }
     }
+
+    if (!durations.isEmpty()) {
+      long sum = 0;
+      long min = Long.MAX_VALUE;
+      long max = 0;
+      for (Long d: durations) {
+        if (d < min)
+          min = d;
+        if (d > max)
+          max = d;
+        sum += d;
+      }
+      long avg = new Double(sum / durations.size()).longValue();
+      this.verifyingStats  = "   number: " + durations.size() + "\n";
+      this.verifyingStats += "    total: " + formatDuration(sum) + "\n";
+      this.verifyingStats += "  average: " + formatDuration(avg) + "\n";
+      this.verifyingStats += "  minimum: " + formatDuration(min) + "\n";
+      this.verifyingStats += "  maximum: " + formatDuration(max) + "\n";
+    }
     return validProportions;
   }
 
-  public LinkedList<String> computeEquations() throws IncompatibleSolutionException {
+  public LinkedList<String> computeEquations() throws IncompatibleSolutionException, MissingTranslationException {
+    LinkedList<Long> durations = new LinkedList<Long>();
     LinkedList<String> productiveEquations = new LinkedList<String>();
 
     for (int i = 0; i < words.size(); i++) {
@@ -112,15 +136,44 @@ public class Reasoner{
           Word wk = words.get(k);
           TupleEquation<IEMLUnit> e = new TupleEquation<IEMLUnit>(wi, wj, wk);
           for (Solution<Tuple<IEMLUnit>> s: e.uniqueSolutions()) {
-            productiveEquations.add(wi.mixedTranslation("fr", 0, dict) + " : "
-                + wj.mixedTranslation("fr", 0, dict) + " :: "
-                + wk.mixedTranslation("fr", 0, dict) + " : "
+            long start = System.currentTimeMillis();
+            productiveEquations.add(this.dict.getFromUSL(wi.getUsl()).get("fr") + "\n" + wi.getUsl() + "\n\t : \n"
+                + this.dict.getFromUSL(wj.getUsl()).get("fr") + "\n" + wj.getUsl() + "\n\t :: \n"
+                + this.dict.getFromUSL(wk.getUsl()).get("fr") + "\n" + wk.getUsl() + "\n\t : \n"
                 + Word.reFactory(s.getContent()).mixedTranslation("fr", 0, dict).prettyPrint(2));
+            durations.add(System.currentTimeMillis() - start);
           }
         }
       }
     }
+
+    if (!durations.isEmpty()) {
+      long sum = 0;
+      long min = Long.MAX_VALUE;
+      long max = 0;
+      for (Long d: durations) {
+        if (d < min)
+          min = d;
+        if (d > max)
+          max = d;
+        sum += d;
+      }
+      long avg = new Double(sum / durations.size()).longValue();
+      this.solvingStats  = "    total: " + formatDuration(sum) + "\n";
+      this.solvingStats += "   number: " + durations.size() + "\n";
+      this.solvingStats += "  average: " + formatDuration(avg) + "\n";
+      this.solvingStats += "  minimum: " + formatDuration(min) + "\n";
+      this.solvingStats += "  maximum: " + formatDuration(max) + "\n";
+    }
     return productiveEquations;
+  }
+
+  public String getVerifyingStats() {
+    return this.verifyingStats;
+  }
+
+  public String getSolvingStats() {
+    return this.solvingStats;
   }
 
 
@@ -136,11 +189,25 @@ public class Reasoner{
     } catch (IOException e) {
       throw new RuntimeException("Cannot open IEML JSON exports. Please generate them first.", e);
     }
+    
+    System.out.println("Displaying current database:");
+    for (Word w: r.words) {
+      System.out.println(w.mixedTranslation("fr", 0, r.dict).get("translations") + " = " + w.getUsl());
+    }
+    System.out.println();
 
     System.out.println("The following french words are used in multiple IEML translations:");
-    for (Entry<String, LinkedList<String>> mapping: r.searchPolysemy("fr").entrySet()) {
-      System.out.println(mapping.getKey() + ": " + mapping.getValue());
+    for (Entry<String, LinkedList<Word>> mapping: r.searchPolysemy("fr").entrySet()) {
+      System.out.println(mapping.getKey() + ":");
+      for (Word w: mapping.getValue())
+        System.out.println("\t" + w.getUsl() + " (" + r.dict.getFromUSL(w.getUsl()).get("fr") + ")");
     }
+    System.out.println();
+
+
+    System.out.println("Solving equations in word database:");
+    for (String solvedEquation: r.computeEquations())
+      System.out.println(solvedEquation + "\n\n");
     System.out.println();
 
 
@@ -149,10 +216,38 @@ public class Reasoner{
       System.out.println(proportion);
     System.out.println();
 
+    System.err.println("Equation solving stats:\n" + r.getSolvingStats());
+    System.err.println("Proportion verification stats:\n" + r.getVerifyingStats());
+  }
 
-    System.out.println("Solving equations in word database:");
-    for (String solvedEquation: r.computeEquations())
-      System.out.println(solvedEquation);
-    System.out.println();
+  public static String formatDuration(long millisInterval) {
+    long abs = Math.abs(millisInterval);
+    String str;
+    if (millisInterval == 0)
+      return "0";
+    else {
+      str = (abs % 1000) + "ms";
+      abs = abs / 1000;
+      if (abs > 0) {
+        str = (abs % 60) + "s " + str;
+        abs = abs / 60;
+        if (abs > 0) {
+          str = (abs % 60) + "m " + str;
+          abs = abs / 60;
+          if (abs > 0) {
+            str = (abs % 24) + "h " + str;
+            abs = abs / 24;
+            if (abs > 0) {
+              str = (abs % 365.25) + "d " + str;
+              abs = (long) Math.floor(abs / 365.25);
+              if (abs > 0) {
+                str = "y " + str;
+              }
+            }
+          }
+        }
+      }
+    }
+    return millisInterval < 0 ? "-" + str : str;
   }
 }
